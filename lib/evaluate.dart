5c20c00bc632ffbcb2b9208ed32fcac865df1854
bryanbill <emobrya@gmail.com>
2023-07-31 01:00:25 +0300

    adds evaluator

--- lib/evaluate.dart
+++ lib/evaluate.dart
@@ -1,4 +1,38 @@
-Future<dynamic> evaluate(Map<String, dynamic> obj) async {
+Future<dynamic> evaluate(Map<String, dynamic> ast) async {
+  if (ast['type'] == 'Program') {
+    return await evaluate(ast['body'][0]);
+  }
+
+  if (ast['type'] == 'CallExpression') {
+    var callee = ast['name'];
+    var args = ast['params'].map((arg) async => await evaluate(arg));
+    if (callee == 'add') {
+      return (await args.first) + (await args.last);
+    }
+    if (callee == 'subtract') {
+      return (await args.first) - (await args.last);
+    }
+    if (callee == 'multiply') {
+      return (await args.first) * (await args.last);
+    }
+    if (callee == 'divide') {
+      return (await args.first) / (await args.last);
+    }
+  }
+
+  if (ast['type'] == 'NumberLiteral') {
+    return double.parse(ast['value']);
+  }
+
+  if (ast['type'] == 'StringLiteral') {
+    return ast['value'];
+  }
+
+  if (ast['type'] == 'Identifier') {
+    return ast['name'];
+  }
+
+  throw Exception("${ast['type']} is not a valid type.");
 }
 
 Object pluckDeep(Map<String, dynamic> obj, String path) =>
