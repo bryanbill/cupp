496f996b0b0fe188025588e112e5a03588c6fe19
bryanbill <emobrya@gmail.com>
2023-07-31 09:32:26 +0300

    adds the std library

---
+++ lib/std_library.dart
@@ -0,0 +1,20 @@
+var environment = {
+  'add': add,
+  'subtract': subtract,
+  'multiply': multiply,
+  'divide': divide,
+  'modulo': modulo,
+  'pow': pow,
+  'max': max,
+};
+
+all(Function(dynamic, dynamic) fn) => (List list) => list.reduce(fn);
+
+add(params) => params['left'] + params['right'];
+subtract(a, b) => a - b;
+multiply(a, b) => a * b;
+divide(a, b) => a / b;
+modulo(a, b) => a % b;
+pow(a, b) => a ^ b;
+
+max(List list) => list.reduce((a, b) => a > b ? a : b);
