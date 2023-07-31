9346fe2ff5bef65ee2a855e220b3047747d8b9c4
bryanbill <emobrya@gmail.com>
2023-07-31 20:26:28 +0300

    parse...

--- lib/parse.dart
+++ lib/parse.dart
@@ -1,64 +1,47 @@
-Map<String, dynamic> parse(List<Map<String, dynamic>> tokens) {
-  var cursor = 0;
+import 'package:cupp/tokenize.dart';
+import 'package:cupp/utilities.dart';
 
-  Map<String, dynamic> walk() {
-    var token = tokens[cursor];
-    if (token['type'] == 'name') {
-      cursor++;
-      return {
-        'type': 'Identifier',
-        'name': token['value'],
-      };
-    }
-
-    if (token['type'] == 'number') {
-      cursor++;
-      return {
-        'type': 'NumberLiteral',
-        'value': token['value'],
-      };
-    }
-
-    if (token['type'] == 'string') {
-      cursor++;
-      return {
-        'type': 'StringLiteral',
-        'value': token['value'],
-      };
-    }
-
-    if (token['type'] == 'parenthesis' && token['value'] == '(') {
-      token = tokens[++cursor];
-
-      var node = {
-        'type': 'CallExpression',
-        'name': token['value'],
-        'params': [],
-      };
-
-      token = tokens[++cursor];
-
-      while ((token['type'] != 'parenthesis') ||
-          (token['type'] == 'parenthesis' && token['value'] != ')')) {
-        node['params'].add(walk());
-        token = tokens[cursor];
-      }
-
-      cursor++;
-      return node;
-    }
-
-    throw Exception("${token['type']} ${token['value']} is not a valid token.");
+Map<String, dynamic> parse(tokens) {
+  print(tokens);
+  tokens = parenthesize(tokens);
+  if (tokens is List) {
+    var [first, ...rest] = tokens;
+    return {
+      "type": "CallExpression",
+      "name": first['value'],
+      "params": rest.map((token) => parse(token)).toList()
+    };
   }
 
-  var ast = {
-    'type': 'Program',
-    'body': [],
-  };
+  var token = tokens;
 
-  while (cursor < tokens.length) {
-    (ast['body'] as List).add(walk());
+  if (token['type'] == 'number') {
+    return {"type": "NumberLiteral", "value": token['value']};
   }
 
-  return ast;
+  if (token['type'] == 'string') {
+    return {"type": "StringLiteral", "value": token['value']};
+  }
+
+  if (token['type'] == 'name') {
+    return {"type": "Identifier", "name": token['value']};
+  }
+
+  return {};
+}
+
+dynamic parenthesize(List tokens) {
+  var token = pop(tokens);
+
+  print("Popped token: $token");
+
+  if (isOpeningParenthesis(token['value'].toString())) {
+    var expression = <dynamic>[];
+    while (!isClosingParenthesis(peek(tokens)['value'].toString())) {
+      expression.add(parenthesize(tokens));
+    }
+    pop(tokens);
+    return expression;
+  }
+  return token;
 }
