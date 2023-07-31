4140ceba77e6391892f49f8c053efc17223311e7
bryanbill <emobrya@gmail.com>
2023-07-31 11:55:16 +0300

    adds special forms

---
+++ lib/special_forms.dart
@@ -0,0 +1,12 @@
+Map<String, Function> specialForms = {
+  "let": let,
+  "final": finalVar,
+};
+
+Map<String, dynamic> let(params) {
+  return {};
+}
+
+Map<String, dynamic> finalVar(params) {
+  return {};
+}
