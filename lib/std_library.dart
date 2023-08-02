689488a1f3f85a3dcaa3b702a60e2e278b361782
bryanbill <emobrya@gmail.com>
2023-08-02 11:18:16 +0300

    adds log in std library

--- lib/std_library.dart
+++ lib/std_library.dart
@@ -6,6 +6,7 @@ var environment = {
   'modulo': modulo,
   'pow': pow,
   'max': max,
+  'log': log,
 };
 
 all(Function(dynamic, dynamic) fn) => (List list) => list.reduce(fn);
@@ -14,10 +15,22 @@ add(params) =>
     params['left'] + (params['right'] as List).reduce((a, b) => a + b);
 subtract(params) =>
     params['left'] - (params['right'] as List).reduce((a, b) => a + b);
-multiply(params) => params['left'] *
-    (params['right'] as List).reduce((a, b) => a * b);
+multiply(params) =>
+    params['left'] * (params['right'] as List).reduce((a, b) => a * b);
 divide(a, b) => a / b;
 modulo(a, b) => a % b;
 pow(a, b) => a ^ b;
 
 max(List list) => list.reduce((a, b) => a > b ? a : b);
+
+log(Map params) {
+  var left = params['left'];
+  var right = params['right'];
+
+  if (right.isEmpty) {
+    print(left.toString());
+  }
+
+  var joined = "$left ${right.reduce((v, e) => '$v $e')}";
+  print(joined);
+}
