31d95f61eb7e040d8cafd76e73f54861bb6b9ecb
bryanbill <emobrya@gmail.com>
2023-08-16 11:53:06 +0300

    throws exception with null final assignments

--- lib/special_forms.dart
+++ lib/special_forms.dart
@@ -9,6 +9,7 @@ var specialForms = {
     if ((node['args'] as List).length > 1) {
       value = Map.from((node['args'] as List)[1]);
     }
+
     node['type'] = 'VariableDeclaration';
     node['identifier'] = identifier;
     node['assignment'] = value;
@@ -19,15 +20,11 @@ var specialForms = {
   },
   "final": (Map node) {
     var identifier = (node['args'] as List)[0];
-    var value = <String, dynamic>{
-      "type": "Literal",
-      "value": null,
-    };
-
-    if ((node['args'] as List).length > 1) {
-      value = Map.from((node['args'] as List)[1]);
+    if (!((node['args'] as List).length > 1)) {
+      throw Exception("final requires an assignment");
     }
-    
+    var value = Map.from((node['args'] as List)[1]);
+
     node['type'] = 'VariableDeclaration';
     node['identifier'] = identifier;
     node['assignment'] = value;
